b551cef0a11d855ca325e87c005fd3fe3d47aee0
Randy Thai <randythai1996@gmail.com>
2017-12-29 13:23:25 -0600

    fix naming

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name='saucenaopy',
-  version='0.1.11',
+  version='0.1.12',
   description='Python SauceNAO API wrapper',
   url='https://github.com/ranthai/saucenaopy',
   author='ranthai',
